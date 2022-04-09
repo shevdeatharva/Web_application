d9f208648f70e7aea54851f4003958f91d652d20
Atharva Anil Shevde <89701795+shevdeatharva@users.noreply.github.com>
2022-04-10 05:09:14 +0530

    intro to redux
    using the provider from react-redux to access the redux store inside the app.js

--- App.js
+++ App.js
@@ -3,19 +3,22 @@ import React from "react";
 import Main from "./component/MainComponent";
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social';
-import { BrowserRouter } from "react-router-dom";
 import "./App.css"
-
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import {Provider} from 'react-redux'
+import {configureStore} from '../src/redux/configureStore'
+const store= configureStore();
 function App() {
+
     return (
+        <Provider store={store}>
 <BrowserRouter>
         <div >
-         <Main/> 
-        {/* <List/> */}
-
-        </div>
-        </BrowserRouter>
-
+          <Main/>  
+         {/* <List/>  */}
+    </div>
+    </BrowserRouter>
+    </Provider>
     )
 }
 
